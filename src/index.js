7d237c1e7d49a8a657dcb09bd39e367647ea8d14
JacobABrennan <jacobabrennan@gmail.com>
2019-06-26 12:56:37 -0400

    Add css reset

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@
 //-- Dependencies --------------------------------
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './reset.css';
 import './index.css';
 import Chat from './chat';
 
